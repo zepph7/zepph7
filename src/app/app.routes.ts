8a21c331c5d46427f32633e27c858a6601b3d628
Zephania Aminieli <87483665+zepph7@users.noreply.github.com>
2026-09-22 11:53:07 +0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-// import { headerComponent } from './header/header.component';
-
-export const routes: Routes = [
-    // {path:'header',component:headerComponent}
-];
